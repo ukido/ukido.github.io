0ab28e2fd2ca65af931d9a599745c2b5b4fe9261
ukido <dev@ukido.io>
2024-12-21 23:51:44 -0500

    welcome page blocks

--- src/app/pages/common/footer/footer.component.ts
+++ src/app/pages/common/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatDividerModule } from "@angular/material/divider";
 import { MatIconModule } from "@angular/material/icon";
@@ -6,6 +6,7 @@ import { MatListModule } from "@angular/material/list";
 import { RouterLink } from "@angular/router";
 import { TranslateModule } from "@ngx-translate/core";
 import { AppSvgIconDirective } from "../icon/svgicon.component";
+import { NavbarItems } from "@app/data/model/ui.model";
 
 
 @Component({
@@ -16,5 +17,8 @@ import { AppSvgIconDirective } from "../icon/svgicon.component";
     styleUrl: './footer.component.scss'
 })
 export class AppFooterComponent {
+
+    @Input()
+    navItems: NavbarItems | null = [];
 
 }
